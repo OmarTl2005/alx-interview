3334543b9320dbf1d9638f0714eebe7d2bd8e47a
OmarTl2005 <hiba41145@gamil.com>
2024-02-15 19:30:37 +0100

    done

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, async (error, response, body) => {
+    error && console.log(error);
+
+    const movies = (JSON.parse(response.body).characters);
+
+    for (const movie of movies) {
+        await new Promise((resolve, reject) => {
+            request(movie, (error, response, body) => {
+                error && console.log(error);
+
+                console.log(JSON.parse(response.body).name);
+                resolve();
+            })
+        })
+    }
+})
